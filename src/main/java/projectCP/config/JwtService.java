d0fce608711aabaa06de4c9b79c1903f56359c0d
Sidharth Nair <realsid6@gmail.com>
2026-03-13 12:12:02 -0400

    fix: jwt validation bug

--- src/main/java/projectCP/config/JwtService.java
+++ src/main/java/projectCP/config/JwtService.java
@@ -53,7 +53,7 @@ public class JwtService {
     }
     public boolean isTokenValid(String token, UserDetails userDetails) {
         final String userName =  extractUserEmail(token);
-        return (userName.equals(userDetails.getUsername()));
+        return (userName.equals(userDetails.getUsername())) && !isTokenExpired(token);
     }
     private boolean isTokenExpired(String token) {
         return extractExpiration(token).before(new Date());
